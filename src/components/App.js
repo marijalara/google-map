c83a3fb74e58211b0696d2059e432f644f86d46f
Marija Majstorovic <majstorovicmarija93@gmail.com>
2023-02-02 15:04:05 +0100

    Fixed key and added container

--- src/components/App.js
+++ src/components/App.js
@@ -71,13 +71,16 @@ const WrappedMap=withScriptjs(withGoogleMap(Map))
 
 const App=() => {
     return(
-        <div style={{ weight: "100vw", height: "100vh"}}>
+        <div className="container">
+                <h1>Temperature in Serbia</h1>
+            <div className="maps" >
             <WrappedMap 
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3key=${process.env.REACT_APP_GOOGLE_KEY}.exp&libraries=geometry,drawing,places&callback=Function.prototype`}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&callback=Function.prototype&key=${process.env.REACT_APP_GOOGLE_KEY}`}
                 loadingElement={<div style={{ height: '100%' }} />}
-                containerElement={<div style={{ height: '700px' }} />}
+                containerElement={<div style={{ height: '680px' }} />}
                 mapElement={<div style={{ height: '100%' }} />}
             />
+        </div>
         </div>
     )
 }
